ee8e078cb6bf958dfa4b15c72df3fc2d2228cd62
Mbalentlediko <dikombalentle9@gmail.com>
2024-05-14 14:09:52 +0200

    Structure

---
+++ script/Exercise2.js
@@ -0,0 +1,13 @@
+let firstName = 'Mbalentle'
+lastName = 'Diko'
+
+let fullName = firstName+' '+lastName
+
+let Numbervar= 5
+
+console.log(Numbervar*5);
+
+const Name = 'Mbalentle'
+Name= 'Mbalentle'
+
+// I got an error
